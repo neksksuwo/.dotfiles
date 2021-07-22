dc097c93d50e7c19a17f2c74bdab8d9432520ee6
neksksuwo <87607337+neksksuwo@users.noreply.github.com>
2021-07-22 17:29:28 +0300

    Create createDirs.sh

---
+++ createDirs.sh
@@ -0,0 +1,20 @@
+# /bin/sh
+
+cd 
+mkdir code
+cd code
+mkdir python pascal shell c cpp asm
+cd 
+mkdir books
+cd books
+mkdir bigBrainStuff otherThings
+cd 
+mkdir music
+mkdir pictures
+cd pictures
+mkdir wallpapers memes otherMemes 
+cd 
+mkdir videos wideos
+mkdir information
+mkdir idk
+mkdir suckless
